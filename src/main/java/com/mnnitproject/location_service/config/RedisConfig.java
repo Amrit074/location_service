23ce3f22584f45189a5c15e66731fa69b5db2b94
Amrit Raj <amritrajar074@gmail.com>
2025-12-31 22:39:06 +0530

    Dockerizing finalized

--- src/main/java/com/mnnitproject/location_service/config/RedisConfig.java
+++ src/main/java/com/mnnitproject/location_service/config/RedisConfig.java
@@ -5,7 +5,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -15,14 +14,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(); // default to localhost:6379
-    }
-
-    @Bean
-    public RedisTemplate<String, LocationResponse> redisTemplate() {
+    public RedisTemplate<String, LocationResponse> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, LocationResponse> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+        template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
