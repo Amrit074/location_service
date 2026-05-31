8e838c3adcdb2c83e8632896f3ead6612962088e
Amrit Raj <amritrajar074@gmail.com>
2026-05-31 16:15:42 +0530

    Avoid hard dependency on Redis cache

--- src/main/java/com/mnnitproject/location_service/service/GPSService.java
+++ src/main/java/com/mnnitproject/location_service/service/GPSService.java
@@ -1,9 +1,7 @@
 package com.mnnitproject.location_service.service;
 
 import com.mnnitproject.location_service.dto.LocationResponse;
-import org.springframework.cache.annotation.Cacheable;
 
 public interface GPSService {
-    @Cacheable(value = "gpsLocationCache", key = "#latitude + '-' + #longitude")
     LocationResponse findLocationByGPS(Double latitude, Double longitude);
 }
--- src/main/java/com/mnnitproject/location_service/service/GeoIPService.java
+++ src/main/java/com/mnnitproject/location_service/service/GeoIPService.java
@@ -1,9 +1,7 @@
 package com.mnnitproject.location_service.service;
 
 import com.mnnitproject.location_service.dto.LocationResponse;
-import org.springframework.cache.annotation.Cacheable;
 
 public interface GeoIPService {
-    @Cacheable(value = "ipLocationCache", key = "#ipAddress")
     LocationResponse findLocationByIp(String ipAddress);
 }
